d890ef831b8e906f93de600e0ac40d90298dfe0c
mejdi <majditaleb@gmail.com>
2015-02-22 06:48:15 +0100

    c

---
+++ ReservationHotel/src/main/java/com/reservation/rdv/web/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.reservation.rdv.web.controllers;
+
+import org.springframework.stereotype.Controller; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.servlet.ModelAndView; 
+@Controller 
+public class MainController { 
+@RequestMapping("index.htm") 
+public ModelAndView main() { 
+ModelAndView mov = new ModelAndView(); 
+mov.setViewName("main"); 
+return mov; 
+} 
+}
